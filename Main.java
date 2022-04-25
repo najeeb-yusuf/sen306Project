762b92eb66397a669d286209bedceba969077ebb
najeeb-yusuf <yusufnajlawal@gmail.com>
2022-04-25 22:34:10 +0100

    First name and last name

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
-import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         studentRecord system = new studentRecord();
-        system.assignStudent("23802","GEN 102");
+        system.printSummary();
     }
 }
